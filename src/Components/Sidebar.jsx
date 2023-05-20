67f90ed1f4567a23c5d740925d85bc801630d08b
Moamen Hamed <moamen.hamed33322@gmail.com>
2023-05-20 03:06:36 +0200

    imgs declare

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -5,11 +5,11 @@ export default function Sidebar() {
   return (
     <aside>
       <ul>
-        <li><a href="#"><img className="home" src="/src/imgs/home-active.png" alt="" /><span>Home</span></a></li>
-        <li><a href="#"><img className="shorts" src="/src/imgs/shorts.png" alt="" /><span>Shorts</span></a></li>
-        <li><a href="#"><img className="subs" src="/src/imgs/subs.png" alt="" /><span>Subscriptions</span></a></li>
-        <li><a href="#"><img className="liberary" src="/src/imgs/liberary.png" alt="" /><span>Library</span></a></li>
-        <li><a href="#"><img className="downloads" src="/src/imgs/downloads.png" alt="" /><span>Downloads</span></a></li>
+        <li><a href="#"><img className="home" src="/imgs/home-active.png" alt="" /><span>Home</span></a></li>
+        <li><a href="#"><img className="shorts" src="/imgs/shorts.png" alt="" /><span>Shorts</span></a></li>
+        <li><a href="#"><img className="subs" src="/imgs/subs.png" alt="" /><span>Subscriptions</span></a></li>
+        <li><a href="#"><img className="liberary" src="/imgs/liberary.png" alt="" /><span>Library</span></a></li>
+        <li><a href="#"><img className="downloads" src="/imgs/downloads.png" alt="" /><span>Downloads</span></a></li>
       </ul>
     </aside>
   )
